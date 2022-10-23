3bfa708f11a1bfc1f44d36d6539c50bd86aa1ada
danjkim21 <dan.jkim21@gmail.com>
2022-10-23 13:14:08 -0400

    updated reactor data cards

--- src/components/OverviewCards.jsx
+++ src/components/OverviewCards.jsx
@@ -13,6 +13,39 @@ function OverviewCards({ reactorData }) {
           </div>
         </div>
       </div>
+
+      <div className="card__reactorData">
+        <h3 className="card__title">Reactor Type</h3>
+        <div className="subcontainer__cardDetail">
+          <i class="fa-solid fa-atom reactorData__icon"></i>
+          <div>
+            <p className="reactorData__detail">{reactorData[0].type}</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="card__reactorData">
+        <h3 className="card__title">Elec. Output, MWe</h3>
+        <div className="subcontainer__cardDetail">
+          <i class="fa-regular fa-lightbulb reactorData__icon"></i>
+          <div>
+            <p className="reactorData__detail">
+              {reactorData[0].outputGross} (gross)
+            </p>
+            <p className="reactorData__detail">{reactorData[0].outputNet} (net)</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="card__reactorData">
+        <h3 className="card__title">Thermal Output, MWth</h3>
+        <div className="subcontainer__cardDetail">
+          <i class="fa-solid fa-fire-flame-simple reactorData__icon"></i>
+          <div>
+            <p className="reactorData__detail">{reactorData[0].thermalOutput}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
