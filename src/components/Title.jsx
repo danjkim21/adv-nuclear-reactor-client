e22fb37ac31136c635364a30f82a381a799be39e
danjkim21 <dan.jkim21@gmail.com>
2022-12-18 12:49:38 -0700

    updated metatags and fontawesome react icons

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,11 +1,13 @@
 import '../assets/App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBolt } from '@fortawesome/free-solid-svg-icons';
 
 function Title({ appTitle }) {
   return (
     <div className='container container__appTitle'>
       <h1 className='header__appTitle'>{appTitle}</h1>
       <span className='container container__icon'>
-        <i className='fa-solid fa-bolt'></i>
+        <FontAwesomeIcon className='app__icon' icon={faBolt} />
       </span>
     </div>
   );
