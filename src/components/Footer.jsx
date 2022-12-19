53c2e8ac57ab3baad81c35ecbf0dacee980b1ff5
danjkim21 <dan.jkim21@gmail.com>
2022-12-19 16:40:46 -0700

    updated footer component name

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import '../assets/App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBolt } from '@fortawesome/free-solid-svg-icons';
 
-function Header() {
+function Footer() {
   return <footer className='section__footer'></footer>;
 }
 
-export default Header;
+export default Footer;
