fb17686b08f30607b03e161fc42a17b84f94b841
danjkim21 <dan.jkim21@gmail.com>
2022-12-19 16:33:39 -0700

    created draft footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import '../assets/App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBolt } from '@fortawesome/free-solid-svg-icons';
+
+function Header() {
+  return <footer className='section__footer'></footer>;
+}
+
+export default Header;
