f94f65f7c3e1e1a95ebf0b9f970219fe3f65312a
danjkim21 <dan.jkim21@gmail.com>
2022-12-20 09:59:09 -0700

    updated footer flex layout and added small element

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,33 +6,45 @@ import {
   faLinkedin,
   faTwitterSquare,
 } from '@fortawesome/free-brands-svg-icons';
-import { faBolt, faLink } from '@fortawesome/free-solid-svg-icons';
+import {
+  faBolt,
+  faExternalLinkSquare,
+  faLink,
+} from '@fortawesome/free-solid-svg-icons';
 
 function Footer() {
   return (
     <footer className='section__footer'>
-      <Link className='mainApp__link' to='/'>
-        <FontAwesomeIcon className='app__icon' icon={faBolt} /> arDB
-      </Link>
-      <div className='container__socials'>
-        <a
-          className='socials__link'
-          href='https://github.com/danjkim21/adv-nuclear-reactor-api-v2'
-        >
-          <FontAwesomeIcon className='socials__icon' icon={faGithubSquare} />
-        </a>
-        <a
-          className='socials__link'
-          href='https://www.linkedin.com/in/danieljayyoungkim/'
-        >
-          <FontAwesomeIcon className='socials__icon' icon={faLinkedin} />
-        </a>
-        <a className='socials__link' href='https://twitter.com/devdanielk'>
-          <FontAwesomeIcon className='socials__icon' icon={faTwitterSquare} />
-        </a>
-        <a className='socials__link' href='https://daniel-jkim.com/'>
-          <FontAwesomeIcon className='socials__icon' icon={faLink} />
-        </a>
+      <div className='row__footer'>
+        <Link className='mainApp__link' to='/'>
+          <FontAwesomeIcon className='app__icon' icon={faBolt} /> arDB
+        </Link>
+        <div className='container__socials'>
+          <a
+            className='socials__link'
+            href='https://github.com/danjkim21/adv-nuclear-reactor-api-v2'
+          >
+            <FontAwesomeIcon className='socials__icon' icon={faGithubSquare} />
+          </a>
+          <a
+            className='socials__link'
+            href='https://www.linkedin.com/in/danieljayyoungkim/'
+          >
+            <FontAwesomeIcon className='socials__icon' icon={faLinkedin} />
+          </a>
+          <a className='socials__link' href='https://twitter.com/devdanielk'>
+            <FontAwesomeIcon className='socials__icon' icon={faTwitterSquare} />
+          </a>
+          <a className='socials__link' href='https://daniel-jkim.com/'>
+            <FontAwesomeIcon
+              className='socials__icon'
+              icon={faExternalLinkSquare}
+            />
+          </a>
+        </div>
+      </div>
+      <div className='row__small'>
+        <small className='small'>© 2022 arDB | Daniel Jay-Young Kim</small>
       </div>
     </footer>
   );
