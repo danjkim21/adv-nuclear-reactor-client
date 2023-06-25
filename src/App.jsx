f1997f97b3bfe1b3952c73cf431251e6d87ea757
danjkim21 <dan.jkim21@gmail.com>
2023-06-25 17:20:37 -0400

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './styles/App.scss';
 import AppRoutes from './router';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <div className='App'>
       <AppRoutes />
+      <Analytics />
     </div>
   );
 }
