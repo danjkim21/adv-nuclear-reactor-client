39b9a1256f0e31684c42ceea6796d4a66c49d110
Daniel J. Kim <dan.jkim21@gmail.com>
2023-11-15 13:30:45 -0500

    Base Authentication/Login Backend Integration (#19)
    * added local storage custom hook
    
    * draft login endpoint implementation to submit func
    
    * updated uselocalstorage hook and implementation
    
    * handle navigate on login
    
    * add basic form validation and login function
    
    * updated app routing with neting and Outlet component
    
    * added draft dashboard page and routing
    
    * added comments for future tasks and enhancements

--- src/components/login-form/index.tsx
+++ src/components/login-form/index.tsx
@@ -1,61 +1,95 @@
 import { useState } from 'react';
 import './LoginForm.scss';
+import { useLocalStorage } from '@uidotdev/usehooks';
+import { useNavigate } from 'react-router-dom';
 
 export default function LoginForm() {
+  const navigate = useNavigate();
   const [formData, setFormData] = useState<any>({
     username: '',
     password: '',
-    redirectTo: null,
   });
+  const [userData, setUserData] = useLocalStorage<any>('userData', null);
+  const [errorMessage, setErrorMessage] = useState('');
 
   const handleChange = (e) => {
+    // Add form validation
     setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    console.log('handleSubmit: ', formData);
+
+    // TODO: Move submit/login function into a custom hook
+    try {
+      const response = await fetch(`https://ardb.cyclic.app/auth/login`, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username: formData.username,
+          password: formData.password,
+        }),
+      });
+
+      if (!response.ok || response.status !== 200) {
+        // Expand statusText's in backend
+        // throw new Error(response.statusText);
+        throw new Error('Invalid username or password');
+      }
+
+      const json = await response.json();
+
+      // Valid login > save user data to local storage and navigate to dashboard
+      setUserData({ username: json.username, authenticated: true });
+      navigate('/dashboard');
+    } catch (error) {
+      if (error instanceof Error) {
+        setErrorMessage(error.message);
+      }
+    }
   };
 
   return (
-    <form className='form-login' onSubmit={handleSubmit}>
-      <div className='form-group'>
-        <label className='form-label' htmlFor='username'>
-          Email Address
-        </label>
-        <input
-          className='form-input'
-          id='username'
-          type='text'
-          name='username'
-          placeholder='Enter Email Address'
-          value={formData.username}
-          onChange={handleChange}
-        />
-      </div>
-      <div className='form-group'>
-        <label className='form-label' htmlFor='password'>
-          Password
-        </label>
-        <input
-          className='form-input'
-          id='password'
-          type='password'
-          name='password'
-          placeholder='Enter Password'
-          value={formData.password}
-          onChange={handleChange}
-        />
-      </div>
-      <div className='form-group'>
-        <input
-          className='form-submit'
-          type='submit'
-          value='Login'
-          // Remove disabled once backed is ready
-          disabled
-        />
-      </div>
-    </form>
+    <>
+      <form className='form-login' onSubmit={handleSubmit}>
+        <div className='form-group'>
+          <label className='form-label' htmlFor='username'>
+            Email Address
+          </label>
+          <input
+            className='form-input'
+            id='username'
+            type='text'
+            name='username'
+            placeholder='Enter Email Address'
+            required
+            value={formData.username}
+            onChange={handleChange}
+          />
+        </div>
+        <div className='form-group'>
+          <label className='form-label' htmlFor='password'>
+            Password
+          </label>
+          <input
+            className='form-input'
+            id='password'
+            type='password'
+            name='password'
+            placeholder='Enter Password'
+            required
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </div>
+        <div className='form-group'>
+          <input className='form-submit' type='submit' value='Login' />
+        </div>
+      </form>
+      {errorMessage && <span className='alert-error'>{errorMessage}</span>}
+    </>
   );
 }
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,19 +1,36 @@
-import { Route, Routes } from 'react-router-dom';
+import { Outlet, Route, Routes } from 'react-router-dom';
+import { useLocalStorage } from '@uidotdev/usehooks';
+
 import Header from '../components/header';
 import Landing from '../pages/landing';
 import About from '../pages/about';
 import Login from '../pages/login';
 import Reactors from '../pages/reactors';
+import Dashboard from '../pages/dashboard';
 
 export default function AppRoutes() {
+  const [userData, setUserData] = useLocalStorage<any>('userData', null);
+
   return (
     <>
-      <Header />
       <Routes>
-        <Route path='/' element={<Landing />} />
-        <Route path='/reactors' element={<Reactors />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/dev' element={<Login />} />
+        {/* Client Site */}
+        <Route
+          element={
+            <>
+              <Header />
+              <Outlet />
+            </>
+          }
+        >
+          <Route path='/' element={<Landing />} />
+          <Route path='/reactors' element={<Reactors />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/login' element={<Login />} />
+        </Route>
+
+        {/* Authenticated Users Site */}
+        <Route path='/dashboard' element={<Dashboard />} />
       </Routes>
     </>
   );
