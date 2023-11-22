4b6c3c48c7d69500186456c9765e2b0ef0fd1186
danjkim21 <dan.jkim21@gmail.com>
2023-11-22 17:20:31 -0600

    added base dashboard table template

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,11 +1,13 @@
 import { Box } from "@mui/material";
 import NavBar from "../../components/nav-bar";
+import ReactorTable from "./components/reactor-table";
 
 export default function Dashboard() {
   return (
     <Box component="main" sx={{ p: 3 }}>
       <NavBar />
       <h1>Dashboard</h1>
+      <ReactorTable />
     </Box>
   );
 }
