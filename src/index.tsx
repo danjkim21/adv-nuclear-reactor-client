caaec2c9919e768c8c606f47ae3b3dd9b0238d33
danjkim21 <dan.jkim21@gmail.com>
2023-11-23 11:08:47 -0600

    moved theme provider to separate file

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './styles/index.scss';
-import './styles/normalize.scss';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles/index.scss";
+import "./styles/normalize.scss";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./styles/theme";
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement as HTMLElement);
 
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <App />
+      </Router>
+    </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+import { createTheme } from "@mui/material";
+import { blue, deepPurple } from "@mui/material/colors";
+
+export const theme = createTheme({
+  palette: {
+    primary: deepPurple,
+    secondary: blue,
+  },
+});
