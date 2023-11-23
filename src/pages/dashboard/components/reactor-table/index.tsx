281628aebf51107ecd4e2f8454e386695bb09b0c
danjkim21 <dan.jkim21@gmail.com>
2023-11-23 11:00:46 -0600

    updated draft table with data and loading indicator

--- src/pages/dashboard/components/reactor-table/index.tsx
+++ src/pages/dashboard/components/reactor-table/index.tsx
@@ -1,37 +1,41 @@
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import useGetAllReactors from "../../../../hooks/useGetAllReactors";
+import { CircularProgress } from "@mui/material";
 
 const columns: GridColDef[] = [
-  { field: "id", headerName: "ID", width: 70 },
-  { field: "firstName", headerName: "First name", width: 130 },
-  { field: "lastName", headerName: "Last name", width: 130 },
+  { field: "name", headerName: "Name", width: 130 },
+  { field: "fullName", headerName: "Full Name", width: 250 },
   {
-    field: "age",
-    headerName: "Age",
-    type: "number",
-    width: 90,
+    field: "type",
+    headerName: "Type",
+    width: 80,
   },
-];
-
-const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  {
+    field: "coolant",
+    headerName: "Coolant",
+    width: 130,
+  },
+  { field: "designOrg", headerName: "Designer", width: 160 },
+  {
+    field: "country",
+    headerName: "Country",
+    width: 130,
+  },
 ];
 
 export default function ReactorTable() {
   const { data: reactorsData, isLoading } = useGetAllReactors();
 
+  const rows = reactorsData.map((reactor) => {
+    return { ...reactor, id: reactor._id };
+  });
+
   return (
-    <div style={{ height: 400, width: "100%" }}>
-      <DataGrid rows={rows} columns={columns} checkboxSelection />
-      {/* <DataGrid rows={reactorsData} columns={columns} checkboxSelection /> */}
+    <div style={{ height: "65vh", width: "100%" }}>
+      {isLoading && <CircularProgress />}
+      {!isLoading && (
+        <DataGrid rows={rows} columns={columns} checkboxSelection />
+      )}
     </div>
   );
 }
