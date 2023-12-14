bb98cf55622ce1991a1a0fe6cea07881f69c3b5e
danjkim21 <dan.jkim21@gmail.com>
2023-12-14 12:15:52 +0800

    added all reactor api queries

--- src/api/reactorsApi.ts
+++ src/api/reactorsApi.ts
@@ -1,8 +1,31 @@
+import { ReactorInterface } from '../types/reactors';
+
 const API_URL = 'https://ardb.cyclic.app/api/';
 
 export const getReactors = async () => {
   const response = await fetch(`${API_URL}`);
-  const data = await response.json();
+  const data: ReactorInterface[] = await response.json();
+
+  return data;
+};
+
+export const getReactorById = async (id: string) => {
+  const response = await fetch(`${API_URL}${id}`);
+  const data: ReactorInterface = await response.json();
+
+  return data;
+};
+
+export const getReactorByType = async (type: string) => {
+  const response = await fetch(`${API_URL}categories/${type}`);
+  const data: ReactorInterface[] = await response.json();
+
+  return data;
+};
+
+export const getReactorTypes = async () => {
+  const response = await fetch(`${API_URL}categories`);
+  const data: string[] = await response.json();
 
   return data;
 };
